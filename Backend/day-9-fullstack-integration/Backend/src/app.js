5886c3307f3490e730a82d092302ae5d1a295ef4
HIDED <hitendewangan100j@gmail.com>
2026-04-14 11:21:08 +0530

    fullstack deployment

--- Backend/day-9-fullstack-integration/Backend/src/app.js
+++ Backend/day-9-fullstack-integration/Backend/src/app.js
@@ -7,6 +7,7 @@
 const express = require('express');
 const Note = require("./models/note.model");
 const cors = require("cors");
+const path = require('path');   // built-in module in nodejs to work with file and directory paths
 
 
 const app = express();
@@ -15,6 +16,14 @@ app.use(cors());   // use cors middleware to allow cross-origin requests from fr
 // middleware to parse json data from request body
 app.use(express.json());
 
+/* 
+ * http://localhost:3000/assets/index-CfhcIE38.js
+ * http://localhost:3000/assets/index-CigruAPO.css
+ * http://localhost:3000/assets/index-notfoundfile.js ,(this will be handled by the catch-all route handler defined at the end of this file, it will send the index.html file for any unmatched route, this is useful for frontend routing in a single-page application (SPA)
+ * 
+ */
+
+app.use(express.static(path.join(__dirname, '../public')));   // serve static files from the public directory, this is where our frontend build files will be located
 
 /* 
  * - POST /api/notes -> create a note
@@ -62,17 +71,16 @@ app.patch("/api/notes/:id", async (req, res) => {
     res.status(200).json({ message: "Note updated successfully" });
 }); 
 
-module.exports = app;
-
-
-
-
-
-
-
-
-
-
+app.use('*name', (req, res) => {
+    // res.status(404).json({ message: "Route not found" });
+    // this is a catch-all route handler, it will be executed if no other route matches the incoming request
+    // also called a wildcard route handler, it should be defined at the end of all route definitions
+    res.sendFile(path.join(__dirname, '../public', 'index.html'));   // send the index.html file for any unmatched route, this is useful for frontend routing in a single-page application (SPA)
+});
 
 
 module.exports = app;
+
+
+
+
